57ba17bd5fec574aa429fb966ca2a685c3ca7c17
Fatih Topal <hemtopal@gmail.com>
2023-06-18 20:06:27 +0300

    08/06/2023

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,49 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+public abstract class TestBase {
+    // bu class'a extends ettigimiz test classlarindan ulasabiliriz.
+    // TestBase clss'indan obje olusturulmasinin onune gecilmesi icin abstrauct yapilabilir.
+
+    protected WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //driver.quit();
+    }
+
+    public void  bekle(int saniye){
+        try {
+            Thread.sleep(saniye*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
+
+
